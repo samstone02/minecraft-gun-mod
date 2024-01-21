e6f5ef517daecc305d357e5f1ee84941d0d4a427
Sam <samuelrystone@gmail.com>
2024-01-21 17:15:24 -0500

    feat: shattering and cracking blocks
    Changes:
    1. Fragile blocks (glass, glowstone) are destroyed
    2. Crackable blocks (dirt, stone, stone bricks) turn into their cracked variant
    3. Bolt shot has durability

--- src/main/java/name/musket_mod/entities/RoundShotEntity.java
+++ src/main/java/name/musket_mod/entities/RoundShotEntity.java
@@ -4,11 +4,15 @@ import name.musket_mod.Entities;
 import name.musket_mod.Items;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.item.HoeItem;
 import net.minecraft.item.Item;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 
-public class RoundShotEntity extends ThrownItemEntity {
+public class RoundShotEntity extends ThrownItemEntity implements MusketShotEntity {
 	public static final Factory FACTORY = new Factory();
 	public static final float SHOT_SPEED = 2.0f;
 	public RoundShotEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
@@ -20,6 +24,25 @@ public class RoundShotEntity extends ThrownItemEntity {
 	@Override
 	protected Item getDefaultItem() {
 		return Items.ROUND_SHOT;
+	}
+	@Override
+	public void onCollision(HitResult hitResult) {
+		if (this.getWorld().isClient) {
+			// NOTE: If we don't ensure that the server thread is running this function
+			// then we can get rendering bugs where there is no breaking particles when firing the shots.
+			// For some reason, setting the environment type to server works,
+			// but the shot entity is not discarded upon collision. Don't ask my why.
+			return;
+		}
+		super.onCollision(hitResult);
+		this.discard();
+	}
+	@Override
+	public void onBlockHit(BlockHitResult hitResult) {
+		super.onBlockHit(hitResult);
+		if (this.getOwner() == null) return; // on world start, if an entity exists then it will cause crash since it has no owner
+		MusketShotEntity.super.onBlockHit(hitResult, this.getWorld(),
+				(this.getOwner().isPlayer())? (PlayerEntity)this.getOwner() : null);
 	}
 	public static class Factory implements EntityType.EntityFactory<RoundShotEntity> {
 		@Override
--- src/main/java/name/musket_mod/entities/ShellShotPelletEntity.java
+++ src/main/java/name/musket_mod/entities/ShellShotPelletEntity.java
@@ -4,11 +4,14 @@ import name.musket_mod.Entities;
 import name.musket_mod.Items;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 
-public class ShellShotPelletEntity extends ThrownItemEntity {
+public class ShellShotPelletEntity extends ThrownItemEntity implements MusketShotEntity {
 	public static final ShellShotPelletEntityFactory FACTORY = new ShellShotPelletEntityFactory();
 	public static final float SHOT_SPEED = 1.0f;
 	public ShellShotPelletEntity(EntityType<? extends ThrownItemEntity> type, World world) {
@@ -16,6 +19,24 @@ public class ShellShotPelletEntity extends ThrownItemEntity {
 	}
 	public ShellShotPelletEntity(EntityType<? extends ThrownItemEntity> type, LivingEntity owner, World world) {
 		super(type, owner, world);
+	}
+	public void onCollision(HitResult hitResult) {
+		if (this.getWorld().isClient) {
+			// NOTE: If we don't ensure that the server thread is running this function
+			// then we can get rendering bugs where there is no breaking particles when firing the shots.
+			// For some reason, setting the environment type to server works,
+			// but the shot entity is not discarded upon collision. Don't ask my why.
+			return;
+		}
+		super.onCollision(hitResult);
+		this.discard();
+	}
+	@Override
+	public void onBlockHit(BlockHitResult hitResult) {
+		super.onBlockHit(hitResult);
+		if (this.getOwner() == null) return; // on world start, if an entity exists then it will cause crash since it has no owner
+		MusketShotEntity.super.onBlockHit(hitResult, this.getWorld(),
+				(this.getOwner().isPlayer())? (PlayerEntity)this.getOwner() : null);
 	}
 	@Override
 	protected Item getDefaultItem() {
